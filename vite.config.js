5c82d7affc04f5ad1ab37135be8f67b464f169ca
NEHA-PATI <nehapati11122004@gmail.com>
2025-08-09 00:16:44 +0530

    final

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
+
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    tailwindcss()
+  ],
   server: {
-    host: true, // needed for Render
-    strictPort: false, // let the server choose an available port
-  },
-});
+      allowedHosts: 'all',
+  },  
+})
