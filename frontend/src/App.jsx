ced1c7d584c1eeb2bbf831711f1b7021334c2a13
amir faishal <amirfaishal484@gmail.com>
2025-08-09 11:50:18 +0530

    framer motion

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -24,6 +24,7 @@ import Memorygame from './pages/MemoryGame/Memory';
 import Activities from './activities';
 import ActivityDetail from './activityDetail';
 import Careers from './pages/Careers';
+
 import CaseStudy from './pages/CaseStudy';
 
 const App = () => {
